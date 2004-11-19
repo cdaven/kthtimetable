815fb7a1b8b3fb7eb8a1528615eb44502d1645b5
cyner <>
2004-11-19 19:27:46 +0000

    flyttade configparser från settings.py till configparser.py och utökade funktionaliteten till att stödja unicode

---
+++ configparser.py
@@ -0,0 +1,42 @@
+# -*- coding: iso-8859-1 -*-
+
+# Skapat av Christian Davén 2004
+
+import ConfigParser
+
+class ConfigParserX(ConfigParser.SafeConfigParser):
+    """
+        En något modifierad variant av "INI-filsläsare".
+
+        Filerna är på typen
+
+            [sektion]
+            nyckel = värde
+    """
+
+    import re
+
+    # gör vänsterledet "case sensitive" och låter
+    # det också innehålla ':' (avgränsare måste vara '=')
+    # eftersom kursnamn kan innehålla ':'.
+    OPTCRE = re.compile(
+        r'(?P<option>[^=\s][^=]*)'
+        r'\s*(?P<vi>[=])\s*'
+        r'(?P<value>.*)$'
+        )
+
+    # istället för att fälten ska skrivas med gemener
+    # skrivs de helt enkelt som de är (i strängrepresentation)
+    # (pekar om funktionen optionxform(option) till str(option))
+    optionxform = str
+
+    # en wrapper-metod som returnerar värdet som Unicode
+    def get(self, section, key):
+        val = ConfigParser.SafeConfigParser.get(self, section, key)
+        return unicode(val, "latin_1")
+
+    # en wrapper-metod som sparar Unicode-värdet som Latin-1
+    def set(self, section, key, value):
+        if isinstance(value, unicode):
+            value = value.encode("latin_1")
+        ConfigParser.SafeConfigParser.set(self, section, key, value)
